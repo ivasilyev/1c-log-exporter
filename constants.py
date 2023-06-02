0defdf352d4d526b90d4f9e2c8951df0575e5560
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2023-06-02 22:55:45 +0300

    Added web-related constants

--- constants.py
+++ constants.py
@@ -1,4 +1,8 @@
 
 AVAILABLE_LOGGING_LEVELS = tuple(range(6))
-TIME_REGEX = "^[0-9]{2}:[0-9]{2}\.[0-9]{6}"
 LOG_ENCODING = "utf-8-sig"
+LOGGING_TEMPLATE = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+JOB_PUSH_DELAY_SECONDS = 3600
+SINGLE_PUSH_DELAY_SECONDS = 3
+SINGLE_PUSH_ATTEMPTS = 100
+TIME_REGEX = "^[0-9]{2}:[0-9]{2}\.[0-9]{6}"
